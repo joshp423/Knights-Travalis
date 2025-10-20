643bed4dd4479dc1fb8dac4417f153cef353ce06
joshp423 <205880399+joshp423@users.noreply.github.com>
2025-10-21 11:02:03 +1300

    finished knightMoves function using BFS
    deleted extra unnecessary files

--- knightMoves.js
+++ knightMoves.js
@@ -1,34 +1,56 @@
 import { createDecisionArray } from "./createDecisionArray.js";
 
+function isVisited(visited, target){
+  for (let coord of visited) {
+    if (coord[0] === target[0] && coord[1] === target[1]) {
+      return true;
+    }
+  }
+  return false;
+};
+
 export function knightMoves(input, target){
-  if (input.includes(target)) {
-      return null;
+
+  //set up queue as input and then nested path we took to get there
+  const queue = [[input, [input]]];
+  
+  //set up visited array to avoid infinite loop on bad paths
+  const visited = [];
+
+  while (queue.length > 0) {
+    
+    // set up current and path variables to store what comes off the queue as above
+    // this is so I can use current to keep track and return path when complete
+    const [current, path] = queue.shift();
+    
+    // check if we have visited before, if so skip to next loop
+    if (isVisited(visited, target)) continue;
+
+    //push current to visited
+    visited.push(current)
+
+    //if current is the target stop the loop and return the path
+    if (current[0] === target[0] && current[1] === target[1]){
+      return path;
     }
 
-  const decisions = createDecisionArray(input)
-  const queue
+    //load valid decisions of current into variable
+    const decisions = createDecisionArray(current)
 
-      //starting from initial decisions array visit coords then put visited coords in seperate array
-      //base case
-      const recursiveStep = () => {
+    //loop through variable and if it has not been visited push it to the queue
+    for (let decision of decisions) {
+      if (!isVisited(visited, decision)){
 
-        if (decisions.length === 0) {
-          return;
-        }
-
-        const current = NodesQueue.shift();
-        if (current.left) {
-          NodesQueue.push(current.left);
-        }
-        if (current.right) {
-          NodesQueue.push(current.right);
-        }
-
-        callback(current);
-        recursiveStep();
-
-  console.log(input)
-
-  knightMoves(decisions, target);
+        // use spread "..." operator to copy all elements to the new array
+        // then add decision on the end as its the next in the path
+        //queue now equals [[next in queue decision], [existing path, next in queue decision]
+        queue.push([decision, [...path, decision]])
       }
+    }
+  }
+
+  //return null if no paths - should never happen
+  return null;
 };
+
+
--- main.js
+++ main.js
@@ -1,15 +1,6 @@
 import { createDecisionArray } from "./createDecisionArray.js";
 import { knightMoves } from "./knightMoves.js";
 // console.log(grid);
-const input = [[3, 3]]
+const input = [0, 0]
 
-console.log(input)
-console.log(knightMoves(input, [4, 3]))
-
-// const NodesQueue = [createDecisionArray(input)];
-// console.log(NodesQueue);
-// vertexData = [0, 1, 2, 3, 4, 5, 6, 7];
-
-// function knightMoves(startingCoords, intendedcoords) {
-//     new tree
-// }
+console.log(knightMoves(input, [7, 7]))
